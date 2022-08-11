b60e7a03f2dca965a6773d989bb55476a3a7544a
Erick <erickeaton7@gmail.com>
2022-08-11 14:38:36 -0400

    Updates

---
+++ precache-manifest.51b767cd277b65fd82f3d11d5dd618e5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "62b08e67c93d254503a5",
+    "url": "/erickeaton7.github.io/static/css/main.1a3fc0bf.chunk.css"
+  },
+  {
+    "revision": "62b08e67c93d254503a5",
+    "url": "/erickeaton7.github.io/static/js/main.f2f17bdf.chunk.js"
+  },
+  {
+    "revision": "c785130e105ccba50be5",
+    "url": "/erickeaton7.github.io/static/js/runtime~main.8e3612d3.js"
+  },
+  {
+    "revision": "ac757767922933a1a2ad",
+    "url": "/erickeaton7.github.io/static/js/2.385fb64b.chunk.js"
+  },
+  {
+    "revision": "da5c74176ead967eb741dba07ef10f67",
+    "url": "/erickeaton7.github.io/static/media/resume.da5c7417.pdf"
+  },
+  {
+    "revision": "5aaadb7320757593789eff5878299b68",
+    "url": "/erickeaton7.github.io/static/media/me-about.5aaadb73.jpg"
+  },
+  {
+    "revision": "347e591413a2e1a991699a57a318da01",
+    "url": "/erickeaton7.github.io/static/media/portfolio1.347e5914.png"
+  },
+  {
+    "revision": "c4f6bf83bccbff573a472baf8e3adc5a",
+    "url": "/erickeaton7.github.io/static/media/portfolio2.c4f6bf83.PNG"
+  },
+  {
+    "revision": "e92448cf6561f47102876fd2d4623b9c",
+    "url": "/erickeaton7.github.io/static/media/portfolio3.e92448cf.PNG"
+  },
+  {
+    "revision": "0d6650bf35e09c525fa4a49114a082ff",
+    "url": "/erickeaton7.github.io/static/media/me.0d6650bf.png"
+  },
+  {
+    "revision": "b89d8f2bd6b776446c23cc71ac587c0a",
+    "url": "/erickeaton7.github.io/index.html"
+  }
+];
